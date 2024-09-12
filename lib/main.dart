a804d2ecff6e9b18a242a72ef63c7c4f790cc3a6
peerapattop <peerapatza401@gmail.com>
2024-09-12 23:25:28 +0700

    read from bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_learn/src/bloc/counter_a_bloc/counter_a_bloc.dart';
 import 'package:flutter_bloc_learn/src/pages/home_page.dart';
 
 void main() {
@@ -10,16 +12,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    final counterABloc =
+    BlocProvider<CounterABloc>(create: (context) => CounterABloc());
+    // final counterABloc =
+    // BlocProvider<CounterABloc>(create: (context) => CounterABloc());
+    return MultiBlocProvider(
+      providers: [
+        counterABloc
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter BLOC'),
       ),
-      home: const MyHomePage(title: 'Flutter BLOC'),
     );
   }
 }
-
-
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_learn/src/bloc/counter_a_bloc/counter_a_bloc.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,7 +12,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,12 +23,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'CouterA',
-            ),
             Text(
-              '',
+              'CounterA:',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            BlocBuilder<CounterABloc, CounterAState>(
+              builder: (context, state) {
+                return Text(
+                  '${state.count}',style: const TextStyle(fontSize: 30),
+                );
+              },
             ),
           ],
         ),
@@ -36,19 +41,18 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: (){},
+            onPressed: () => context.read<CounterABloc>().add(CounterAEventAdd()),
             tooltip: 'Add',
             child: const Icon(Icons.add),
           ),
           const SizedBox(height: 20),
           FloatingActionButton(
-            onPressed: (){},
+            onPressed: () => context.read<CounterABloc>().add(CounterAEventReset()),
             tooltip: 'Reset',
             child: const Icon(Icons.remove),
           ),
         ],
       ),
-
     );
   }
 }
