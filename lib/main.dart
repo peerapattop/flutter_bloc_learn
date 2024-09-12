002bfb29565ae81799d9a250f5754dc1719f8c51
peerapattop <peerapatza401@gmail.com>
2024-09-12 22:31:07 +0700

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/src/pages/home_page.dart';
+import 'package:flutter_bloc_learn/src/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
